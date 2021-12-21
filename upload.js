a5e64b9d0ef61ee5297ee8bfac6b117a2e980472
DavidGit999 <itprog999@gmail.com>
2021-12-21 22:11:33 +0200

    Convert bytes to size

--- upload.js
+++ upload.js
@@ -1,3 +1,12 @@
+function bytesToSize(bytes) {
+  var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+  if (!bytes) {
+    return '0 Byte';
+  }
+  const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+  return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
+}
+
 export function upload(selector, options = {}) {
   const input = document.querySelector(selector);
   const preview = document.createElement('div')
@@ -43,6 +52,10 @@ export function upload(selector, options = {}) {
         <div class="preview-image">
           <div class="preview-remove">&times;</div>
           <img src="${src}" alt="${file.name}" />
+          <div class="preview-info">
+            <span>${file.name}</span>
+            ${bytesToSize(file.size)}
+          </div>
         </div>
         `)
       }
