6ff019d036caaf794d89a57c66c5cca3172dbb1c
DavidGit999 <itprog999@gmail.com>
2021-12-21 16:48:34 +0200

    Fix image positioning and duplication

--- upload.js
+++ upload.js
@@ -1,5 +1,8 @@
 export function upload(selector, options = {}) {
   const input = document.querySelector(selector);
+  const preview = document.createElement('div')
+
+  preview.classList.add('preview')
 
   const open = document.createElement('button')
   open.classList.add('btn')
@@ -13,6 +16,7 @@ export function upload(selector, options = {}) {
     input.setAttribute('accept', options.accept.join(', '))
   }
 
+  input.insertAdjacentElement('afterend', preview)
   input.insertAdjacentElement('afterend', open)
 
   const triggerInput = () => input.click()
@@ -24,6 +28,7 @@ export function upload(selector, options = {}) {
 
     const files = Array.from(event.target.files)
 
+    preview.innerHTML = ''
     files.forEach(file => {
       if (!file.type.match('image')) {
         return
@@ -32,8 +37,12 @@ export function upload(selector, options = {}) {
       const reader = new FileReader()
 
       reader.onload = ev => {
-        console.log(ev.target.result)
-        input.insertAdjacentHTML('afterend', `<img src="${ev.target.result}" />`)
+        const src = ev.target.result
+        preview.insertAdjacentHTML('afterbegin', `
+        <div class="preview-image">
+          <img src="${src}" alt="${file.name}" />
+        </div>
+        `)
       }
 
       reader.readAsDataURL(file)
